6afa15bb4dd4b930d681f8134572b20b21fa762d
amahadnezhad <amahadnezhad@gmail.com>
2024-02-27 01:38:17 +0330

    Add SignUp Class

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from .views import SignUpView
+
+
 urlpatterns = [
-    # path('signup/', ,name='signup'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]

09a7cad5623b18947733f076a8b5568d0b20dcc9
amahadnezhad <amahadnezhad@gmail.com>
2024-02-27 18:50:28 +0330

    Add Password Reset Template

--- config/settings.py
+++ config/settings.py
@@ -114,12 +114,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
+# LANGUAGE_CODE = 'fa'
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
